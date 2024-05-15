f4cbd3937b8b11a6bc70d75d80bd4aa3ed0aaef3
Alex-kimathi <alex2kimathi@gmail.com>
2024-05-15 19:26:00 +0000

    Task 3: creating a basemodel

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Creates a base class where other classes will inherit
+    when an instance of the class is called, the parameters are updated
+    Assisgns a unique id, time created and time updated
+    """
+    def __init__(self):
+        self.id = (uuid.uuid4)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    """converts the output of the instance to a string """
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    """Upated the time updated once the instance is saved """
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Returns a dictionary containing all the key or values on __dict__
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
