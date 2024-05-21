936d793902b363d75b3b398a1c3ddc4a9a1f37a5
LeahKamau <kamauleah35@gmail.com>
2024-05-21 09:18:17 -0700

    reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,31 +19,39 @@ class FileStorage:
 
     def all(self):
         """  returns the dictionary __objects """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """ sets in __objects the obj
         key <obj class name>.id """
         key = str(obj.__class__.__name__) + '.' + str(obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file """
         save_dict = {}
-        for key, value in FileStorage.__objects.items():
+        for key, value in self.__objects.items():
             save_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            f.write(json.dumps(save_dict))
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(save_dict, f)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
+        cls_dict = {
+            'BaseModel': BaseModel,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review,
+            'User': User
+        }
         if not (os.path.exists(FileStorage.__file_path)):
             pass
         else:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
                 reload_dict = json.load(f)
 
-                for obj in reload_dict.values():
-                    cname = obj["__class__"]
-                    del obj["__class__"]
-                    self.new(eval(cname)(**obj))
+                for key, value in reload_dict.items():
+                    cname = key.split(".")[0]
+                    self.__objects[key] = cls_dict[cname](**value)
